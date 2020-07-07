3a8919a8274c1238990ac6ba7944c1e2e84a9426
Kevin Lee <kevinleemur@gmail.com>
2020-07-06 22:37:18 -0400

    updated database

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,11 +6,11 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db'not in g:
-        g.db=sqlite3.connect(current_app.config['DATABASE'])
-        detect_types = sqlite3.PARSE_DECLTYPES
-        )
+        g.db=sqlite3.connect(current_app.config['DATABASE'],
+        detect_types = sqlite3.PARSE_DECLTYPES)
 
-        g.db.row_factory= sqlite.Row
+
+        g.db.row_factory= sqlite3.Row
         return g.db
 
 def close_db(e=None):
@@ -25,7 +25,7 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-@click.command('init-db'flasflas)
+@click.command('init-db')
 @with_appcontext
 def init_db_command():
     init_db()
